7059647affd8decc36f2e7ba55e09689d63e3121
Joy <travelerjoy85@gmail.com>
2017-04-17 19:13:01 +0000

    Exercise 2 complete

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-app.get('/hello', function (req, res) {
+// app.use(bodyParser.urlencoded({extended: true}));
+
+// Exercise 1
+/* app.get('/hello', function (req, res) {
   res.send('<h1>Hello World!</h1>');
-});
+}); */
+
+// Exercise 2
+app.get('/hello', function (req, res) {
+  if(req.query.name){
+    res.send("<h1>Hello" + " " + req.query.name);
+  }
+  else{
+    res.send("<h1>Hello World!</h1>");
+  }
+}); 
 
 
 
@@ -15,4 +30,3 @@ app.get('/hello', function (req, res) {
 var server = app.listen(process.env.PORT, process.env.IP, function () {
   console.log('Example app listening at http://%s', process.env.C9_HOSTNAME);
 });
-
